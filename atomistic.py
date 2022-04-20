c7b52498488daa90c0868b125fb1a1fcc2255147
phankl <philipp.kloza@gmail.com>
2022-04-20 14:16:11 +0100

    added input file + updated atomistic data file generation

--- atomistic.py
+++ atomistic.py
@@ -5,6 +5,9 @@ import cnt
 def dataFile(cnt1, cnt2, cellMin, cellMax, fileName):
   sites1 = cnt1.sites
   sites2 = cnt2.sites
+  
+  unitCellSiteNumber1 = len(cnt1.unitCell.sites)
+  unitCellSiteNumber2 = len(cnt2.unitCell.sites)
 
   sites = np.concatenate((sites1, sites2))
 
@@ -17,23 +20,31 @@ def dataFile(cnt1, cnt2, cellMin, cellMax, fileName):
     f.write("  0 dihedrals\n")
     f.write("  0 impropers\n\n")
 
-    f.write("  1 atom types\n")
+    f.write("  3 atom types\n")
     f.write(f"  {cellMin[0]} {cellMax[0]} xlo xhi\n")
     f.write(f"  {cellMin[1]} {cellMax[1]} ylo yhi\n")
     f.write(f"  {cellMin[2]} {cellMax[2]} zlo zhi\n\n")
 
     f.write("Masses\n\n")
 
-    f.write("  1 12.0\n\n")
+    f.write("  1 12.0\n")
+    f.write("  2 12.0\n")
+    f.write("  3 12.0\n")
 
-    f.write("Atom\n\n")
+    f.write("Atoms\n\n")
 
     atomID = 1
-    for site in sites1:
-      f.write(f"{atomID} 1 1 0 {site[0]} {site[1]} {site[2]}\n")
+    for i, site in enumerate(sites1):
+      if i < unitCellSiteNumber1:
+        f.write(f"{atomID} 1 2 0 {site[0]} {site[1]} {site[2]}\n")
+      else:
+        f.write(f"{atomID} 1 1 0 {site[0]} {site[1]} {site[2]}\n")
       atomID += 1
-    for site in sites2:
-      f.write(f"{atomID} 2 1 0 {site[0]} {site[1]} {site[2]}\n")
+    for i, site in enumerate(sites2):
+      if i < unitCellSiteNumber2:
+        f.write(f"{atomID} 2 3 0 {site[0]} {site[1]} {site[2]}\n")
+      else:
+        f.write(f"{atomID} 2 1 0 {site[0]} {site[1]} {site[2]}\n")
       atomID += 1
 
 n = 10
@@ -44,7 +55,7 @@ angle *= np.pi / 180.0
 
 cells = 102
 
-dist = 2.0 * 6.785 + 5
+dist = 2.0 * 6.785 + 3
 
 cnt1 = cnt.CNT(n, m, cells, cellMode=True, axis=(1.0, 0.0, 0.0))
 cnt2 = cnt.CNT(n, n, cells, cellMode=True, origin=(0.0, dist, 0.0), axis=(1.0, 0.0, 0.0))
